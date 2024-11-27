b2ae8fc20d36bcc520bf73bb4586410a70f2c926
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-11-27 11:12:02 +0530

    Stream-API Collect method

--- com/streamapi/practice/Collect.java
+++ com/streamapi/practice/Collect.java
@@ -1,7 +1,19 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Collect {
     public static void main(String[] args){
+        List<String> fruits = Arrays.asList("Orange", "Mango", "Banana", "Orange");
 
+        List<String> newFruits = fruits.stream().collect(Collectors.toList());
+        System.out.println(newFruits);
+
+
+        Set<String> uniqueFruits = fruits.stream().collect(Collectors.toSet());
+        System.out.println(uniqueFruits);
     }
 }
--- com/streamapi/practice/ForEach.java
+++ com/streamapi/practice/ForEach.java
@@ -10,5 +10,7 @@ public class ForEach {
 
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
         numbers.stream().forEach(a->System.out.println(a));
+
+        numbers.stream().forEach(a->System.out.println(a*3));
     }
 }
