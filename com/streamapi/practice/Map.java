48ae5d1905b3c6b6684d9d2502e8b34d87bd7773
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-11-27 11:37:11 +0530

    Stream-API map method

--- com/streamapi/practice/Map.java
+++ com/streamapi/practice/Map.java
@@ -1,7 +1,23 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Map {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
+        List<Integer> sqrNumbers = numbers.stream().map(num-> num*num)
+                                                   .collect(Collectors.toList());
+        System.out.println(sqrNumbers);
+
+
+        List<String> fruits = Arrays.asList("Mango", "Banana", "Orange", "Apple");
+        List<Integer> lengths = fruits.stream()
+                                      .map(String::length)
+                                      .collect(Collectors.toList());
+
+        System.out.println(lengths);
     }
 }
