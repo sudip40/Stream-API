a3c4366146eb8c1a81f4c06a0bd074ebf43fabb4
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-11-29 12:12:24 +0530

    Stream-API count method

--- com/streamapi/practice/Count.java
+++ com/streamapi/practice/Count.java
@@ -1,7 +1,12 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Count {
     public static void main(String[] args) {
-
+        List<String> fruits = Arrays.asList("Mango", "Banana", "Orange", "Apple");
+        Long cont = fruits.stream().count();
+        System.out.println(cont);
     }
 }
