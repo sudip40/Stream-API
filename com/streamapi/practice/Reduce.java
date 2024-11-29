51b760b75d671ef8138d98d2446b8f934fa57357
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-11-29 12:19:41 +0530

    Stream-API reduce method

--- com/streamapi/practice/Reduce.java
+++ com/streamapi/practice/Reduce.java
@@ -1,7 +1,15 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Reduce {
     public static void main(String[] args) {
-
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5, 4, 3, 2, 1);
+        Optional<Integer> sum = numbers.stream().reduce((a, b)-> a + b);
+        if(sum.isPresent()){
+            System.out.println(sum.get());
+        }
     }
 }
