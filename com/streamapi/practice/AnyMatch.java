00876c1dd1dc631025f67649099ecf4a08e0128f
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-11-29 12:39:43 +0530

    Stream-API AllMatch method

--- com/streamapi/practice/AnyMatch.java
+++ com/streamapi/practice/AnyMatch.java
@@ -1,7 +1,15 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AnyMatch {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5, 4, 3, 2, 1);
+        boolean bool = numbers.stream().anyMatch(number->number%2==0);
+        System.out.println(bool);
 
+        boolean bool2 = numbers.stream().anyMatch(number->number%2!=0);
+        System.out.println(bool2);
     }
 }
