70550eb8e869f9671511daf60f05183b350cf1a7
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-11-29 12:14:14 +0530

    Stream-API sorted method

--- com/streamapi/practice/Sorted.java
+++ com/streamapi/practice/Sorted.java
@@ -1,7 +1,15 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Sorted {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5, 4, 3, 2, 1);
 
+        List<Integer> sortedNumbers = numbers.stream().sorted().collect(Collectors.toList());
+
+        System.out.println(sortedNumbers);
     }
 }
