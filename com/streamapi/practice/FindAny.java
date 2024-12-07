f8d867a28892c06189d75fa97691a832ad9d4300
Sudip Karmakar <karmakar.sudip40@gmail.com>
2024-12-07 11:23:50 +0530

    Stream-API FindAny & FindFirst method

--- com/streamapi/practice/FindAny.java
+++ com/streamapi/practice/FindAny.java
@@ -1,7 +1,13 @@
 package com.streamapi.practice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class FindAny {
     public static void main(String[] args) {
-
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
+        Optional<Integer> num = numbers.stream().filter(a->a%2==0).findAny();
+        num.ifPresent(System.out::println);
     }
 }
